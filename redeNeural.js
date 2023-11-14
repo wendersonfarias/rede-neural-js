c4507ad47ffcbdb966f75bbeac0eb624e592f14c
Wenderson Farias <wendersonpfarias@gmail.com>
2023-11-13 21:30:42 -0400

    algoritmo feedfoward

---
+++ redeNeural.js
@@ -0,0 +1,45 @@
+function sigmoide(x) {
+    return 1 / (1 + Math.exp(-x));
+}
+
+class RedeNeural {
+
+    constructor(i_nodes, h_nodes, o_nodes) {
+        this.i_nodes = i_nodes;
+        this.h_nodes = h_nodes;
+        this.o_nodes = o_nodes;
+
+        this.bias_ih = new Matrix(this.h_nodes, 1);
+        this.bias_ih.randomize();
+        this.bias_ho = new Matrix(this.o_nodes, 1);
+        this.bias_ho.randomize();
+
+        this.weights_ih = new Matrix(this.h_nodes, this.i_nodes);
+        this.weights_ih.randomize();
+
+        this.weights_ho = new Matrix(this.o_nodes, this.h_nodes);
+        this.weights_ho.randomize();
+
+
+
+    }
+
+    feedforward(arr) {
+        // INPUT  -> HIDDEN
+
+        let input = Matrix.arrayToMatrix(arr);
+
+        let hidden = Matrix.multiply(this.weights_ih, input);
+        hidden = Matrix.add(hidden, this.bias_ih);
+        hidden.map(sigmoide);
+
+
+        // HIDDEN => OUTPUT
+
+        let output =  Matrix.multiply(this.weights_ho, hidden);
+        output = Matrix.add(output, this.bias_ho);
+        output.print();
+        output.map(sigmoide);
+        output.print();
+    }
+}
